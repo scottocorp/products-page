7e81bb5d8e7ba26833047ad002f449db93135ec8
Scott McDonald <scottocorp@gmail.com>
2025-08-18 03:17:06 +1000

    installing the mantine component library

--- src/ui/layout/index.tsx
+++ src/ui/layout/index.tsx
@@ -1,13 +1,21 @@
 'use client'
 
+import { createTheme, MantineProvider } from '@mantine/core'
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+})
+
 // This is a wrapper that can be used to pass components like <StoreProvider> to the children, 
 // without having to touch the master app/layout.tsx.
 
 export const Layout = ({ children } : { children: any }) => {
 
   return (
-    <div className="p-8">
-      {children}
-    </div>
+      <MantineProvider theme={theme}>
+        <div className="p-8">
+          {children}
+        </div>
+      </MantineProvider>
   )
 }
