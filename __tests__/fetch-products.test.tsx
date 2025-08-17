a5b35fc96c19bf6a8fe352ed59e3ef822f8c6fa6
Scott McDonald <scottocorp@gmail.com>
2025-08-18 03:54:04 +1000

    fixing the test

--- __tests__/fetch-products.test.tsx
+++ __tests__/fetch-products.test.tsx
@@ -3,23 +3,23 @@ import { fetchProducts } from '@/lib'
 
 describe('sum module', () => {
   test('fetches all items', async () => {
-    const products = await fetchProducts('ALL' as any)
+    const products = await fetchProducts('ALL' as any, '')
     expect(products.length).toBe(8)
   })
   test('fetches all items of type "Beer"', async () => {
-    const products = await fetchProducts('BEER' as any)
+    const products = await fetchProducts('BEER' as any, '')
     expect(products.length).toBe(4)
   })
   test('fetches all items of type "Wine"', async () => {
-    const products = await fetchProducts('WINE' as any)
+    const products = await fetchProducts('WINE' as any, '')
     expect(products.length).toBe(2)
   })
   test('fetches all items of type "Cider"', async () => {
-    const products = await fetchProducts('CIDER' as any)
+    const products = await fetchProducts('CIDER' as any, '')
     expect(products.length).toBe(1)
   })
   test('fetches all items of type "Cider"', async () => {
-    const products = await fetchProducts('CIDER' as any)
+    const products = await fetchProducts('CIDER' as any, '')
     expect(products.length).toBe(1)
   })
 })
